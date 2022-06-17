f4683cdd3049151cc1d0480c5ec93fb0ff0de5ce
flavio2018 <flavio.petruzzellis@gmail.com>
2022-06-17 17:19:59 +0200

    Add D-NTM variant builder

--- src/model/builders.py
+++ src/model/builders.py
@@ -2,14 +2,14 @@ from model.archies.CustomLSTM import build_lstm
 from model.archies.CustomMLP import build_mlp
 from model.archies.CustomRNN import build_rnn
 from model.dntm.DynamicNeuralTuringMachine import build_dntm
-from model.dntm.DynamicNeuralTuringMachine import build_dntm as build_masked_dntm
+from model.dntm_variant.DynamicNeuralTuringMachine import build_dntm as build_dntm_variant
 
 
 def build_model(model_conf, device):
     if model_conf.name == 'dntm':
         return build_dntm(model_conf, device)
-    elif model_conf.name == 'masked_dntm':
-        return build_masked_dntm(model_conf, device)
+    elif model_conf.name == 'dntm_variant':
+        return build_dntm_variant(model_conf, device)
     elif model_conf.name == 'mlp':
         return build_mlp(model_conf, device)
     elif model_conf.name == 'rnn':
